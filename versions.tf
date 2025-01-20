8e4f90b4aed936b72b177d2039bbcc6b3a8596a1
Artem Vovchenko <artem_vovchenko@epam.com>
2025-01-20 12:06:36 +0200

    fix: make defining permissions optional

--- versions.tf
+++ versions.tf
@@ -1,11 +1,7 @@
 terraform {
-  required_version = ">=1.0.0"
+  required_version = "~>1.3"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.0.1"
-    }
     databricks = {
       source  = "databricks/databricks"
       version = "~>1.0"
