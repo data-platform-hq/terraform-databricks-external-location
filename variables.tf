c47d00e2c6fa3821be407af969d1e97a0c24443d
Artem Vovchenko <artem_vovchenko@epam.com>
2025-01-22 15:07:34 +0200

    Move create_storage_crednetial parameter to storage_credntial variable

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "storage_credential" {
     owner                     = optional(string)       # Owner of resource
     force_destroy             = optional(bool, true)
     comment                   = optional(string, "Managed identity credential provisioned by Terraform")
+    create_storage_credential = optional(bool, true)   # "Boolean flag that determines whether to create storage credential or use the existing one"
     permissions = optional(set(object({
       principal  = string
       privileges = list(string)
@@ -12,7 +13,6 @@ variable "storage_credential" {
     isolation_mode = optional(string, "ISOLATION_MODE_OPEN")
   })
   description = "Object with storage credentials configuration attributes"
-  default     = {}
 }
 
 variable "cloud" {
@@ -20,18 +20,12 @@ variable "cloud" {
   description = "Cloud (azure, aws or gcp)"
 }
 
-variable "create_storage_credential" {
-  type        = bool
-  default     = true
-  description = "Boolean flag that determines whether to create storage credential or use the existing one"
-}
-
 variable "external_locations" {
   type = list(object({
     index            = string                # Index of instance, for example short name, used later to access exact external location in output map
     name             = string                # Custom whole name of resource
     url              = string                # Path URL in cloud storage
-    credentials_name = optional(string)      # If create_storage_credential is set to false, provide id of existing storage credential here
+    credentials_name = optional(string)      # If storage_credential.create_storage_credential is set to false, provide id of existing storage credential here
     owner            = optional(string)      # Owner of resource
     skip_validation  = optional(bool, true)  # Suppress validation errors if any & force save the external location
     read_only        = optional(bool, false) # Indicates whether the external location is read-only.
